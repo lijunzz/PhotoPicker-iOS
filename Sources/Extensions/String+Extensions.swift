00e865d3020da297c56eb8113ea41b1057794a33
LiJun <29483147+lijunzz@users.noreply.github.com>
2018-05-04 18:18:12 +0800

    添加 CocoaPods 配置文件。

--- Sources/Extensions/String+Extensions.swift
+++ Sources/Extensions/String+Extensions.swift
@@ -28,8 +28,9 @@ extension String {
     /// - Parameters:
     ///   - identifier: 套装 ID，以加载不同模块的语言文件。
     /// - Returns: 本地语言对应的字符串
-    public func language(identifier: AnyClass = ImagePicker.self) -> String {
+    func language() -> String {
         let bundle = Bundle(for: ImagePicker.self)
+        
         return NSLocalizedString(self, bundle: bundle, comment: self)
     }
     
--- Sources/Extensions/UIImage+Extensions.swift
+++ Sources/Extensions/UIImage+Extensions.swift
@@ -1,6 +1,6 @@
 //===--- UIImage+Extensions.swift -----------------------------------------===//
 //
-// Copyright (C) 2018 LiJun
+// Copyright (C) 2018 hucool + LiJun
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
